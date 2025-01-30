b641c13b7b78f2a8d92f13d4ebe737bba9412250
lucashildever <lucaspaulo_212@hotmail.com>
2025-01-30 17:35:22 -0300

    basic header styling

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,13 +1,27 @@
+import styled from 'styled-components';
+
 const Header = () => {
     return (
-        <header>
+        <StyledHeader>
             <ul>
                 <li>Women</li>
                 <li>Men</li>
                 <li>Kid</li>
             </ul>
-        </header>
+        </StyledHeader>
     );
 }
+
+const StyledHeader = styled.header`
+    ul {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+
+        li {
+            cursor: pointer;
+        }
+    }
+`;
 
 export default Header;
