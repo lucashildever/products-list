7be9bbb7733b7d7945779076811883d5edac47cc
lucashildever <lucaspaulo_212@hotmail.com>
2025-01-30 17:34:08 -0300

    router created

---
+++ frontend/src/router.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import ProductDetails from './components/pages/product-details';
+import ProductsListing from './components/pages/products-listing';
+
+const router = createBrowserRouter([
+   {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <ProductsListing />
+            },
+            {
+                path: 'product/:productId',
+                element: <ProductDetails />
+            }
+        ]
+    }
+])
+
+export default router;
