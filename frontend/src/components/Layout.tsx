7b5bbac9158fc671e021473e1c7486448f2f4ecd
lucashildever <lucaspaulo_212@hotmail.com>
2025-03-10 10:19:08 -0300

    prettier adjustment

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router';
-import Header from './header/header';
+import Header from './header/Header';
 
 const Layout = () => {
     return (
@@ -7,7 +7,7 @@ const Layout = () => {
             <Header />
             <Outlet />
         </>
-    )
-}
+    );
+};
 
 export default Layout;
