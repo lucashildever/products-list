9bb0c8222d4b05609a5fdef873e5eba3810b510a
lucashildever <lucaspaulo_212@hotmail.com>
2025-03-10 10:20:19 -0300

    header state and dispatch included

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,100 +1,88 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useHeaderContext } from '../../hooks/useHeaderContext';
+import { HeaderCategory } from '../../types/headerTypes';
 
-import CartButton from './CartButton';
-import CartOverlay from './CartOverlay';
+import CartButton from './cart-button/CartButton';
+import CartOverlay from './cart-overlay/CartOverlay';
 
+import HeaderContainer from './header.styles';
 import logo from '/logo.svg';
 
 const Header = () => {
-
-    const [active, setActive] = useState<string>('women');
+    const { headerState, headerDispatch } = useHeaderContext();
 
     return (
         <>
-            <StyledHeader>
+            <HeaderContainer>
                 <ul>
                     <li
-                        onClick={() => setActive('women')}
-                        className={active === 'women' ? 'active' : ''}
-                        data-testid={active === 'women' ? 'active-category-link' : 'category-link'}
+                        onClick={() =>
+                            headerDispatch({
+                                type: 'CATEGORY_CLICK',
+                                payload: HeaderCategory.ALL,
+                            })
+                        }
+                        className={
+                            headerState.selectedCategory === HeaderCategory.ALL
+                                ? 'active'
+                                : ''
+                        }
+                        data-testid={
+                            headerState.selectedCategory === HeaderCategory.ALL
+                                ? 'active-category-link'
+                                : 'category-link'
+                        }
                     >
-                        <span>Women</span>
+                        <span>All</span>
                     </li>
                     <li
-                        onClick={() => setActive('men')}
-                        className={active === 'men' ? 'active' : ''}
-                        data-testid={active === 'men' ? 'active-category-link' : 'category-link'}
+                        onClick={() =>
+                            headerDispatch({
+                                type: 'CATEGORY_CLICK',
+                                payload: HeaderCategory.CLOTHES,
+                            })
+                        }
+                        className={
+                            headerState.selectedCategory ===
+                            HeaderCategory.CLOTHES
+                                ? 'active'
+                                : ''
+                        }
+                        data-testid={
+                            headerState.selectedCategory ===
+                            HeaderCategory.CLOTHES
+                                ? 'active-category-link'
+                                : 'category-link'
+                        }
                     >
-                        <span>Men</span>
+                        <span>Clothes</span>
                     </li>
                     <li
-                        onClick={() => setActive('kid')}
-                        className={active === 'kid' ? 'active' : ''}
-                        data-testid={active === 'kid' ? 'active-category-link' : 'category-link'}
+                        onClick={() =>
+                            headerDispatch({
+                                type: 'CATEGORY_CLICK',
+                                payload: HeaderCategory.TECH,
+                            })
+                        }
+                        className={
+                            headerState.selectedCategory === HeaderCategory.TECH
+                                ? 'active'
+                                : ''
+                        }
+                        data-testid={
+                            headerState.selectedCategory === HeaderCategory.TECH
+                                ? 'active-category-link'
+                                : 'category-link'
+                        }
                     >
-                        <span>Kid</span>
+                        <span>Tech</span>
                     </li>
                 </ul>
                 <img src={logo} alt="logo" />
                 <CartButton />
-            </StyledHeader>
-            <CartOverlay />
+            </HeaderContainer>
+            <CartOverlay isOpen={headerState.isCartOpen} />
         </>
     );
 };
-
-const StyledHeader = styled.header`
-    position: fixed;
-    top: 0;
-    width: 80%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 70px;
-    background-color: white;
-    /* padding-top: 17px;
-    padding-bottom: 17px; */
-
-    img[alt="logo"] {
-        position: absolute;
-        height: 35px;
-        left: 50%;
-        transform: translateX(-50%);
-    }
-
-    ul {
-        display: flex;
-
-        li { 
-            cursor: pointer;
-            position: relative;
-
-            // middle link spacing
-            &:nth-child(2) {
-                margin-left: 15px;
-                margin-right: 15px;
-            }
-
-            &:after {
-                transition: ease 0.3s;
-                content: '';
-                position: absolute;
-                left: 0;
-                bottom: -15px;
-                width: 100%;
-                height: 2px;
-                background-color: #5ECE7B;
-                opacity: 0;
-            }
-
-            &.active {
-                color : #5ECE7B;
-                &:after { opacity: 1; }
-                font-weight: 600;
-            }
-        }
-    }
-`;
 
 export default Header;
