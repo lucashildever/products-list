74c2878420322fd5a5899ab105e4793bba7bb6c1
lucashildever <lucaspaulo_212@hotmail.com>
2025-03-10 10:24:08 -0300

    prettier adjustments

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,23 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
-import ProductDetails from './components/pages/ProductDetails';
-import ProductsListing from './components/pages/ProductsListing';
+import ProductDetails from './components/pages/product-details/ProductDetails';
+import ProductsListing from './components/pages/product-listing/ProductsListing';
 
 const router = createBrowserRouter([
-   {
+    {
         path: '/',
         element: <Layout />,
         children: [
             {
                 index: true,
-                element: <ProductsListing />
+                element: <ProductsListing />,
             },
             {
                 path: 'product/:productId',
-                element: <ProductDetails />
-            }
-        ]
-    }
+                element: <ProductDetails />,
+            },
+        ],
+    },
 ]);
 
 export default router;
